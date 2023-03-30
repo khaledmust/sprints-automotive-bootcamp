76526d65ad5c5bddc0c7329e8a7e1f65837d3e12
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2023-03-30 12:22:39 +0200

    S_DS_01-check-balanced-parenthesis

--- sprint-2/S_DS_01-check-balanced-parenthesis/main.c
+++ sprint-2/S_DS_01-check-balanced-parenthesis/main.c
@@ -3,8 +3,13 @@
 #include "balanced.h"
 
 int main() {
+    /* Uncomment to test the stack implementation. */
 //    __testStack();
-    uint8_t myString[50] = "{(2+3)/(<3+3>*(15-10))}";
-    isBalancedParanthethes(myString);
+
+    uint8_t user_string[50];
+    printf("Enter the expression to want to check: ");
+    scanf("%s", user_string);
+
+    isBalancedParanthethes(user_string);
     return 0;
 }
